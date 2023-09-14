d618542d49258b893771371fe64833441911f1f4
Divyanshi Awasthi <divyanshiawasthi15@gmail.com>
2023-09-14 21:58:51 +0530

    Add files via upload

---
+++ binarysearch.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int len= nums.length;
+        int b=0;
+        int l=len-1;
+        int mid;
+        while(b<=l)
+        {
+            mid=(b+l)/2;
+            if(target==nums[mid])
+              return mid;
+              else if(target>nums[mid])
+              b=mid+1;
+            else
+              l=mid-1;
+        }
+        return -1;
+    }
+}
