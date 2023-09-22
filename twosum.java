9c7ed5f9a95e55f54002bf98e4fc25ec0719636e
Divyanshi Awasthi <divyanshiawasthi15@gmail.com>
2023-09-23 01:13:06 +0530

    Add files via upload

---
+++ twosum.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap();
+        //fill Hshmap
+        for (int i = 0; i < nums.length; i++) 
+        {
+            map.put(nums[i],i);
+        }
+        //Searching
+         for (int i = 0; i < nums.length; i++) {
+            int t = target - nums[i];
+            if (map.containsKey(t)) {
+                int index= map.get(t);
+                if(index==i)
+                continue;
+                return new int[] {i,index};
+            }
+         }
+
+         return new int[]{};
+    }
+}
