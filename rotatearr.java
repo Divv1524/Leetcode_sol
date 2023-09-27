bca9adefd63ec4b5a672343d8c4efc95e4ce0447
Divyanshi Awasthi <divyanshiawasthi15@gmail.com>
2023-09-28 02:20:24 +0530

    Update rotatearr.java

--- rotatearr.java
+++ rotatearr.java
@@ -1,14 +1,21 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-       k %= nums.length;
-    int temp, previous;
+    int n= nums.length;
+    if (n == 0)
+      return;
+    k = k % n;
+    if (k > n)
+      return;
+    int[] temp = new int[k];
+    for (int i = n - k; i < n; i++) {
+      temp[i - n + k] = nums[i];
+    }
+    for (int i = n - k - 1; i >= 0; i--) {
+      nums[i + k] = nums[i];
+    }
     for (int i = 0; i < k; i++) {
-      previous = nums[nums.length - 1];
-      for (int j = 0; j < nums.length; j++) {
-        temp = nums[j];
-        nums[j] = previous;
-        previous = temp;
+      nums[i] = temp[i];
+
     }
 }
     }
-}
