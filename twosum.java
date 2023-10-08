b27e4625888d8b11e1846ee9902798678f6793bf
Divyanshi Awasthi <divyanshiawasthi15@gmail.com>
2023-10-08 23:41:26 +0530

    Update twosum.java

--- twosum.java
+++ twosum.java
@@ -2,12 +2,11 @@ import java.util.HashMap;
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap();
-        //fill Hshmap
+    
         for (int i = 0; i < nums.length; i++) 
         {
             map.put(nums[i],i);
         }
-        //Searching
          for (int i = 0; i < nums.length; i++) {
             int t = target - nums[i];
             if (map.containsKey(t)) {
