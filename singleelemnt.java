06910200745072f88208333217d644fbfc5fedb5
Divyanshi Awasthi <divyanshiawasthi15@gmail.com>
2023-10-10 00:54:07 +0530

    Add files via upload

---
+++ singleelemnt.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class singleelemnt {
+        public static int singleNonDuplicate(int[] nums) {
+            int n = nums.length; // Size of the array.
+    
+            // Edge cases:
+            if (n == 1)
+            {
+                return nums[0];
+            }
+            if (nums[0]!=nums[1])
+            {
+                return nums[0];
+            }
+            if (nums[n-1]!=nums[n-2])
+                return nums[n-1];
+    
+            int low = 1, high = n - 2;
+            while (low <= high) {
+                int mid = (low + high) / 2;
+    
+                // If arr[mid] is the single element:
+                if (nums[mid]!=nums[mid-1] && nums[mid] != nums[mid+1])
+                {
+                    return nums[mid];
+                }
+                if ((mid % 2 == 1 && nums[mid] == nums[mid - 1])
+                    || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+                //eliminate the left half:
+                low = mid + 1;
+            }
+            //we are in the right:
+            else {
+                //eliminate the right half:
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) 
+    {
+        int[] arr = {1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6};
+        System.out.println(singleNonDuplicate(arr));
+    }
+    }
