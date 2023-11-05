8952b40e644e7bc6e81fba3c8df6fac71e4afffb
Divyanshi Awasthi <divyanshiawasthi15@gmail.com>
2023-11-05 18:54:21 +0530

    Add files via upload

---
+++ kokoeating.java
@@ -0,0 +1,38 @@
+public class kokoeating 
+{
+        public int minEatingSpeed(int[] piles, int h) 
+        {
+             int low = 1, high = findMax(piles);
+    
+            //apply binary search:
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                int totalH = calculateTotalHours(piles, mid);
+                if (totalH <= h) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            return low;
+        }
+         public static int calculateTotalHours(int[] piles, int h) {
+            int totalH = 0;
+            int n = piles.length;
+            //find total hours:
+            for (int i = 0; i < n; i++) {
+                totalH += Math.ceil((double)(piles[i]) / (double)(h));
+            }
+            return totalH;
+        }
+        public static int findMax(int[] piles) {
+            int maxi = Integer.MIN_VALUE;;
+            int n = piles.length;
+            //find the maximum:
+            for (int i = 0; i < n; i++) {
+                maxi = Math.max(maxi, piles[i]);
+            }
+            return maxi;
+        }
+    }
+}
