3ad2602d2a070557d878cd16238d19864a67820a
Divyanshi Awasthi <divyanshiawasthi15@gmail.com>
2023-11-06 23:05:42 +0530

    Add files via upload

---
+++ bsin2dpart2.java
@@ -0,0 +1,30 @@
+public class bsin2dpart2 {
+        public static boolean searchMatrix(int[][] matrix, int target) {
+            int n= matrix.length;
+            int m= matrix[0].length;
+            int low=0, high=n*m-1;
+            int row= 0;
+            int col= m-1;
+            while(row<n && col>=0)
+            {
+                
+                if(matrix[row][col]==target)
+                {
+                    return true;
+                }
+                else if(matrix[row][col]<target)
+                {
+                    row++;
+                }
+                else{
+                    col--;
+                }
+            }
+            return false;
+        }
+        public static void main(String[] args) {
+            int matrix[][]= {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+            int target= 24;
+            System.out.println(searchMatrix(matrix, target));
+    }
+}
