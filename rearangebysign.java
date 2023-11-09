dd172cc9cfa586fbe2995769d88c8bac6aad2919
Divyanshi Awasthi <divyanshiawasthi15@gmail.com>
2023-11-09 23:53:02 +0530

    Add files via upload

---
+++ rearangebysign.java
@@ -0,0 +1,23 @@
+public class Solution {
+  public int[] rearrangeArray(int[] nums) {
+      int n= nums.length;
+      int ans[]= new int[n];
+      int pi=0,ni=1;
+      for(int i=0;i<n;i++)
+      {
+          if(nums[i]<0)
+          {
+              ans[ni]=nums[i];
+              ni+=2;
+          }
+          else
+          {
+              ans[pi]=nums[i];
+              pi+=2;
+          }
+      }
+      return ans;
+  }
+} {
+  
+}
